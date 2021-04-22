24d67971e5e70aee16499353c7212a34e73daaae
OHGUSHI toshifumi <webmaster@tonbi.jp>
2021-04-22 17:04:25 +0900

    戦闘シーンでのキャラ位置の調整

--- TF_ScreenUtil.js
+++ TF_ScreenUtil.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_ScreenUtil.js
-// Version :0.2.3.0
+// Version :0.3.0.0
 // For : RPGツクールMZ (RPG Maker MZ)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2020-2021
@@ -20,6 +20,8 @@
  *
  * @help
  * マップ画面を拡大するとか画面周りのプラグイン。
+ * 
+ * ※ PluginCommonBase 定義によりパラメータや引数に \V[n] を使えます。
  *
  * ●メタタグ
  * [マップ]のメモ欄にメタタグを書き込むと、そのマップはスクロールせず固定となります。
@@ -31,9 +33,8 @@
  * 例: <TF_fixedMap:-2, 0.8>
  *
  * TODO :
- * HalfMove.js の判定位置が下にずれるのを修正。
  * 固定のON/OFFするコマンド
- * 的画像位置の調整ON/OFFパラメータ
+ * 敵画像位置の調整ON/OFFパラメータ
  * 
  * 利用規約 : MITライセンス
  * @================================================
@@ -173,42 +174,6 @@
 		$gameMap.setDisplayPos( parseFloat( args[ 1 ] ), parseFloat( args[ 2 ] ) );
 	};
 
-	/*--- Scene_Title ---*/
-	const _Scene_Title_start = Scene_Title.prototype.start;
-	Scene_Title.prototype.start = function() {
-		_Scene_Title_start.call( this );
-
-		fitToScreen( this._backSprite1 );
-		fitToScreen( this._backSprite2 );
-	};
-
-	/**
-	* スプライトを画面いっぱいに拡大
-	* @param {Sprite} sprite スプライト
-	*/
-	function fitToScreen( sprite ) {
-		if( !sprite.bitmap ) return;
-		if( !sprite.bitmap.width ) {
-			return setTimeout( fitToScreen.bind( this, sprite ), 5 );	// ビットマップの読み込み確認
-		}
-
-		const scaleX = Graphics.width / sprite.bitmap.width;
-		const scaleY = Graphics.height / sprite.bitmap.height;
-		if( 1 < scaleX ) sprite.scale.x = scaleX;
-		if( 1 < scaleY ) sprite.scale.y = scaleY;
-		centerSprite( sprite );
-	}
-	/**
-	 * スプライトを画面中央に表示
-	 * @param {Sprite} sprite スプライト
-	 */
-	function centerSprite( sprite ) {
-		sprite.x = Graphics.width / 2;
-		sprite.y = Graphics.height / 2;
-		sprite.anchor.x = 0.5;
-		sprite.anchor.y = 0.5;
-	}
-
 
 	/*--- Spriteset_Base ---*/
 	// 左上に角を合わせる(振動などに対応する必要はある)
@@ -221,64 +186,35 @@
 		this.x += Math.round( $gameScreen.shake() );
 	};
 
-
-	/*--- Spriteset_Battle ---*/
-	const TYPE_STAGE = 1;	// ステージ(地面)背景
-	const TYPE_SET = 2;			// セット(建物)背景
-	const _Spriteset_Battle_createBattleback = Spriteset_Battle.prototype.createBattleback;
-	Spriteset_Battle.prototype.createBattleback = function() {
-		_Spriteset_Battle_createBattleback.call( this );
-		// this._back1Sprite = new Sprite_Battleback( gameMap.battleback1Name(), TYPE_STAGE );
-		// this._back2Sprite = new Sprite_Battleback( gameMap.battleback2Name(), TYPE_SET );
-		// fitToScreen( this._back1Sprite );
-		// fitToScreen( this._back2Sprite );
-		// this._battleField.addChild( this._back1Sprite );
-		// this._battleField.addChild( this._back2Sprite );
-	};
-	//Spriteset_Battle.prototype.updateBattleback = function() { };
-
-
-	/*--- Sprite_Battleback ---*/
-	class Sprite_Battleback extends Sprite {
-		constructor( bitmapName, type ) {
-			super();
-			this.bitmap = this.createBitmap( bitmapName, type );
-		}
-
-		/**
-		 * 背景画像の生成・読み込み
-		 */
-		createBitmap( bitmapName, type ) {
-			if( bitmapName === "" ) {
-				return new Bitmap( Graphics.width, Graphics.height );
-			} else if( type === TYPE_STAGE ) {
-				return ImageManager.loadBattleback1( bitmapName );
-			} else {
-				return ImageManager.loadBattleback2( bitmapName );
-			}
-		}
-	}
-
-
-	/*--- Sprite_Enemy ---*/
 	const DEFAULT_SCREEN_WIDTH = 816;
 	const DEFAULT_SCREEN_HEIGHT = 624;
-	/**
-	 * 敵位置をスクリーンサイズに合わせて調整
-	 */
-	// const _Sprite_Enemy_setBattler = Sprite_Enemy.prototype.setBattler;
-	// Sprite_Enemy.prototype.setBattler = function( battler ) {
-	// 	_Sprite_Enemy_setBattler.call( this, battler );
 
-	// 	if( !this._enemy._alteredScreenY ) {
-	// 		this._homeY += Math.floor( ( Graphics.height - DEFAULT_SCREEN_HEIGHT ) / 2 );
-	// 		this._enemy._screenY = this._homeY;
-	// 		this._enemy._alteredScreenY = true;
-	// 	}
-	// 	if( $gameSystem.isSideView() || this._enemy._alteredScreenX ) return;
+	/*--- Sprite_Actor ---*/
+	// アクター位置をスクリーンサイズに合わせて調整
+	const _Sprite_Actor_setActorHome = Sprite_Actor.prototype.setActorHome;
+	Sprite_Actor.prototype.setActorHome = function( index ) {
+		_Sprite_Actor_setActorHome.apply( this, arguments );
 
-	// 	this._homeX += ( Graphics.width - DEFAULT_SCREEN_WIDTH ) / 2;
-	// 	this._enemy._screenX = this._homeX;
-	// 	this._enemy._alteredScreenX = true;
-	// };
+		const x = 600 + ( Graphics.boxWidth - DEFAULT_SCREEN_WIDTH ) + index * 32;
+		const y = 280 + ( Graphics.boxHeight - DEFAULT_SCREEN_HEIGHT ) + index * 48;
+		this.setHome( x, y );
+	};
+
+	/*--- Sprite_Enemy ---*/
+	// 敵位置をスクリーンサイズに合わせて調整
+	const _Sprite_Enemy_setBattler = Sprite_Enemy.prototype.setBattler;
+	Sprite_Enemy.prototype.setBattler = function( battler ) {
+		_Sprite_Enemy_setBattler.apply( this, arguments );
+
+		if( !this._enemy._alteredScreenY ) {
+			this._homeY += Math.floor( ( Graphics.height - DEFAULT_SCREEN_HEIGHT ) / 2 );
+			this._enemy._screenY = this._homeY;
+			this._enemy._alteredScreenY = true;
+		}
+		if( $gameSystem.isSideView() || this._enemy._alteredScreenX ) return;
+
+		this._homeX += ( Graphics.width - DEFAULT_SCREEN_WIDTH ) / 2;
+		this._enemy._screenX = this._homeX;
+		this._enemy._alteredScreenX = true;
+	};
 } )();
