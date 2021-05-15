96d5286e81dd1434a44f472669e72c10bf81a07a
OHGUSHI toshifumi <webmaster@tonbi.jp>
2021-05-15 17:52:45 +0900

    タイトルコマンドを追加するパラメータを追加

--- TF_MenuLauncher.js
+++ TF_MenuLauncher.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_MenuLauncher.js
-// Version :0.1.1.1
+// Version :0.2.0.0
 // For : RPGツクールMZ (RPG Maker MZ)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2021
@@ -18,31 +18,88 @@
  * @orderAfter PluginCommonBase
  * @base SceneCustomMenu
  *
+ *
+ * @param rem1 @text ＿＿＿＿ タイトル ＿＿＿＿
+ * @desc タイトルシーンに関する機能。
+ * (なお、これは区切り線なので値を入力しても何も起きません)
+ *
  * @param sceneTitle @text タイトルシーン識別子
  * @desc SceneCustomMenu.js のシーン識別子
- * 空だと通常のメニュー。
+ * 空だと通常のタイトルシーンを表示。
+ * @type string @default
+ * 
+ * @param commonEvIdTitle @text タイトルコモンイベント名
+ * @desc タイトルシーンの代わりにコモンイベントを呼びます。
+ * 必要ならコモンイベント中にタイトルシーンを呼んでください。
  * @type string @default
  *
+ * @param emptyTitle @text タイトル標準コマンド削除
+ * @desc [タイトルシーン識別子]とは共存できません。
+ * 必ず[タイトル追加コマンド]を設定してください。
+ * @type boolean @default false
+ * @on 標準コマンド削除 @off 追記(規定)
+ * 
+ * @param commandTitle @text タイトル追加コマンド
+ * @desc [タイトルシーン識別子]とは共存できません。
+ * @type struct<CommandParam>[] @default
+ *
+ *
+ * @param rem2 @text ＿＿＿＿ メニュー ＿＿＿＿
+ * @desc メニューシーンに関する機能。
+ * (なお、これは区切り線なので値を入力しても何も起きません)
+ * 
  * @param sceneMenu @text メニューシーン識別子
  * @desc SceneCustomMenu.js のシーン識別子
- * 空だと通常のメニュー。
+ * 空だと通常のメニューシーンを表示。
  * @type string @default
+ *
+ * @param commonEvIdMenu @text メニューコモンイベント名
+ * @desc メニューシーンの代わりにコモンイベントを呼びます。
+ * 必要ならコモンイベント中にメニューシーンを呼んでください。
+ * @type string @default
+ *
+ * @param emptyMenu @text メニュー標準コマンド削除
+ * @desc [メニューシーン識別子]とは共存できません。
+ * 必ず[メニュー追加コマンド]を設定してください。
+ * @type boolean @default false
+ * @on 標準コマンド削除 @off 追記(規定)
+ *
+ * @param commandMenu @text メニュー追加コマンド
+ * @desc [メニューシーン識別子]とは共存できません。
+ * @type struct<CommandParam>[] @default
  * 
  *
  * @help
+ * カスタムメニュー作成プラグイン(SceneCustomMenu.js)で作ったシーンを
+ * 通常のタイトルやメニューと入れ替えることができます。
+ * 
  * 実装予定!
  * ・メニューの項目を設定できるようにする。
  * ・エディタでは消せないメニューの項目を消す。
  * ・規定のシーンも選べるようにする。
  * ・任意のキーでシーンを呼び出す。
- * 
- * SceneCustomMenu.js で作ったシーンを通常のメニューの代わりに開く。
- * など、SceneCustomMenu.js の補助的な機能として使えます。
- *
- * 使い方はパラメータの[対象シーン識別子]に
- * SceneCustomMenu.js で作ったシーンのシーン識別子を書くだけ。
+ * ・シーンの代わりにコモンイベントを呼ぶ。
  *
  * 
+ */
+/*~struct~CommandParam:ja
+ * @param label @text コマンドラベル
+ * @desc メニューに表示されるコマンド名。
+ * @type string @default コマンド
+ *
+ * @param sceneId @text シーン識別子
+ * @desc SceneCustomMenu.js のシーン識別子
+ * カスタムメニューを呼びます。
+ * @type string @default
+ *
+ * @param sceneClass @text シーンクラス名
+ * @desc Scene_Base の子孫クラス名
+ * 標準の JavaScriptクラスを呼びます。
+ * @type string @default
+ * 
+ * @param commonEvId @text コモンイベント名
+ * @desc コモンイベントを呼びます。
+ * @type string @default
  */
 ( () => {
     "use strict";
@@ -57,6 +114,67 @@
         console.log( `${PLUGIN_NAME}: args = ${args[ "arg1" ]}` );
     } );
 
+    /*---- Scene_Title ----*/
+    // コマンド数に合わせて、高さを増やす。
+    const _Scene_Title_commandWindowRect = Scene_Title.prototype.commandWindowRect;
+    Scene_Title.prototype.commandWindowRect = function() {
+        const rect = _Scene_Title_commandWindowRect.call( this );
+        // コマンド数に合わせて、高さを再設定
+        const itemLength = ( pluginParams.emptyTitle ? 0 : 3 ) + pluginParams.commandTitle.length;
+        const itemHeight = this.calcWindowHeight( itemLength, true );
+        rect.height = itemHeight;
+        return rect;
+    };
+
+    const _Scene_Title_createCommandWindow = Scene_Title.prototype.createCommandWindow;
+    Scene_Title.prototype.createCommandWindow = function() {
+        if( pluginParams.emptyTitle ) {
+            const background = $dataSystem.titleCommandWindow.background;
+            const rect = this.commandWindowRect();
+            this._commandWindow = new Window_TitleCommand( rect );
+            this._commandWindow.setBackgroundType( background );
+            this.addWindow( this._commandWindow );
+        } else {
+            _Scene_Title_createCommandWindow.call( this );
+        }
+
+        // 設定されたコマンドを追加
+        pluginParams.commandTitle.forEach( e => {
+            this._commandWindow.setHandler( HANDLER_CUSTOM_COMMAND, customCommandTitle.bind( this ) );
+        } );
+    };
+    function customCommandTitle() {
+        const i = this._commandWindow.index() - ( pluginParams.emptyTitle ? 0 : 3 );
+        const command = pluginParams.commandTitle[ i ];
+
+        // indexに沿ってコマンドを処理
+        if( command.sceneId ) {
+            this._windowLayer.destroy();
+            SceneManager.callCustomMenu( command.sceneId );
+        } else if( command.sceneClass ) {
+            this._windowLayer.destroy();
+            SceneManager.push( new Function( `return ${command.sceneClass}` )() );
+        } else if( command.commonEvId ) {
+            // コモンイベントの実行
+            console.log( `コモンイベント : ${command.commonEvId}` );
+
+        }
+    }
+
+    /*---- Window_TitleCommand ----*/
+    const HANDLER_CUSTOM_COMMAND = "handlerCustomCommand";
+    /**
+     * タイトルのメニューにコマンドを追加。
+     */
+    const _Window_TitleCommand_makeCommandList = Window_TitleCommand.prototype.makeCommandList;
+    Window_TitleCommand.prototype.makeCommandList = function() {
+        if( !pluginParams.emptyTitle ) _Window_TitleCommand_makeCommandList.call( this );
+
+        // 設定されたコマンドを追加
+        pluginParams.commandTitle.forEach(
+            e => this.addCommand( e.label, HANDLER_CUSTOM_COMMAND )
+        );
+    };
 
 
 
