27242f5fe93c1c71ff8d445e5c25c2f4aa6bfa25
OHGUSHI toshifumi <webmaster@tonbi.jp>
2021-10-10 19:35:36 +0900

    stringToEvent と各種パラメータパース関数を追加

--- TF_Core.js
+++ TF_Core.js
@@ -58,6 +58,86 @@
         // do something
     };
 
+    /*---- パラメータパース関数 ----*/
+    const PARAM_TRUE = "true";
+    const PARAM_ON = "on";
+    const TYPE_BOOLEAN = "boolean";
+    const TYPE_NUMBER = "number";
+    const TYPE_STRING = "string";
+    /**
+     * 与えられた文字列に変数が指定されていたら、変数の内容に変換して返す。
+     * @param {String} value 変換元の文字列( \V[n]形式を含む )
+     * @return {String} 変換後の文字列
+     */
+    function treatValue( value ) {
+        if( value === undefined || value === "" ) return "0";
+        const result = value.match( /\x1bV\[(.+)\]/i );
+        if( result === null ) return value;
+        const id = parseInt( result[ 1 ], 10 );
+        if( isNaN( id ) ) {
+            return $gameVariables.valueByName( result[ 1 ] );
+        } else {
+            return $gameVariables.value( id );
+        }
+    }
+
+    /*--- Game_Variables ---*/
+    /**
+     * 変数を文字列で指定し、値を返す。
+     * @param {String} name 変数(ID, 名前, \V[n]による指定が可能)
+     */
+    Game_Variables.prototype.valueByName = function( name ) {
+        return this.value( stringToVariableId( name ) );
+    };
+    /**
+     * 指定された変数のIDを返す。
+     * @param {String} name 変数(ID, 名前, \V[n]による指定が可能)
+     */
+    function stringToVariableId( name ) {
+        name = treatValue( name );
+        let i = $dataSystem.variables.findIndex( i => i === name );
+        if( 0 <= i ) return i;
+        i = parseInt( name, 10 );
+        if( isNaN( i ) ) throw Error( `${PLUGIN_NAME}: I can't find the variable '${name}'` );
+        return i;
+    }
+
+    /**
+     * 文字列を整数に変換して返す。
+     * @param {String|Number} value
+     * @return {Number} 数値に変換した結果
+     */
+    function parseIntStrict( value ) {
+        if( typeof value === TYPE_NUMBER ) return Math.floor( value );
+        const result = parseInt( treatValue( value ), 10 );
+        if( isNaN( result ) ) throw Error( `${PLUGIN_NAME}: [${value}] is not a number.` );
+        return result;
+    }
+
+    /**
+     * 文字列を実数に変換して返す。
+     * @param {String|Number} value
+     * @return {Number} 数値に変換した結果
+     */
+    function parseFloatStrict( value ) {
+        if( typeof value === TYPE_NUMBER ) return value;
+        const result = parseFloat( treatValue( value ) );
+        if( isNaN( result ) ) throw Error( `${PLUGIN_NAME}: [${value}] is not a number.` );
+        return result;
+    }
+
+    /**
+     * 文字列を真偽値に変換して返す。
+     * @param {String|Boolean} value 変換元文字列
+     * @returns {Boolean} 
+     */
+    function parseBooleanStrict( value ) {
+        if( typeof value === TYPE_BOOLEAN ) return value;
+        value = treatValue( value );
+        const result = value.toLowerCase();
+        return ( result === PARAM_TRUE || result === PARAM_ON );
+    }
+
 
     /*--- ユーティリティ関数 ---*/
     /**
@@ -67,10 +147,27 @@
      */
     function stringToPoint( pointStr ) {
         const args = pointStr.match( /([-.0-9]+)[^-.0-9]+([-.0-9]+)/ );
-        if( args === null ) throw `${PLUGIN_NAME}: wrong parameter "${pointStr}"`;
+        if( args === null ) throw `${PLUGIN_NAME}: wrong parameter '${pointStr}'`;
         return new Point( parseFloat( args[ 1 ] ), parseFloat( args[ 2 ] ) );
     }
 
+
+    /*--- イベントID・オブジェクト取得関数 ---*/
+    /*---- イベントIDの配置オフセット ----*/
+    const FOLLOWER_OFFSET = -2;
+    const VEHICLE_OFFSET = -100;
+
+    /*---- イベントID変換用文字列 ----*/
+    const EVENT_THIS = "this";
+    const EVENT_PLAYER = "player";
+    const EVENT_FOLLOWER0 = "follower0";
+    const EVENT_FOLLOWER1 = "follower1";
+    const EVENT_FOLLOWER2 = "follower2";
+    const EVENT_FOLLOWER_ALL = "all";
+    const VEHICLE_BOAT = "boat";
+    const VEHICLE_SHIP = "ship";
+    const VEHICLE_AIRSHIP = "airship";
+    const VEHICLE_WALK = "walk";
 
     /**
      * character を拡張して隊列メンバーも指定できるようにしたもの。
@@ -88,21 +185,6 @@
         }
     }
 
-    /*---- イベントIDの配置オフセット ----*/
-    const FOLLOWER_OFFSET = -2;
-    const VEHICLE_OFFSET = -100;
-
-    /*---- イベントID変換用文字列 ----*/
-    const EVENT_THIS = "this";
-    const EVENT_PLAYER = "player";
-    const EVENT_FOLLOWER0 = "follower0";
-    const EVENT_FOLLOWER1 = "follower1";
-    const EVENT_FOLLOWER2 = "follower2";
-    const EVENT_FOLLOWER_ALL = "all";
-    const VEHICLE_BOAT = "boat";
-    const VEHICLE_SHIP = "ship";
-    const VEHICLE_AIRSHIP = "airship";
-    const VEHICLE_WALK = "walk";
     /**
      * 文字列をイベントIDへ変換
      * @param {String} value イベントIDの番号か識別子
@@ -129,3 +211,14 @@
         if( e === undefined ) throw Error( `${PLUGIN_NAME}: I can't find the event '${value}'` );
         return e.id;
     }
+
+    /**
+     * 指定された文字列に対応するイベントを返す
+     * @param {Game_Interpreter} interpreter インタプリタ
+     * @param {String} eventId イベントIDの番号か識別子
+     * @returns {Game_CharacterBase}
+     */
+    function stringToEvent( interpreter, eventId ) {
+        return getEventById( interpreter, stringToEventId( eventId ) );
+    }
+} )();
