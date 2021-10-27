77bf95375410542c89cb89834ce3ac18d0cdb70f
OHGUSHI toshifumi <webmaster@tonbi.jp>
2021-10-27 18:27:39 +0900

    メンバー変更時のアップデートに対応

--- TF_Shadow.js
+++ TF_Shadow.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_Shadow.js
-// Version :0.7.0.0
+// Version :0.7.1.0
 // For : RPGツクールMZ (RPG Maker MZ)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2021
@@ -121,6 +121,20 @@
 
     const TYPE_STRING = "string";
 
+    /*---- イベントIDの配置オフセット ----*/
+    const FOLLOWER_OFFSET = -2;
+    const VEHICLE_OFFSET = -100;
+
+    /*---- イベントID変換用文字列 ----*/
+    const EVENT_THIS = "this";
+    const EVENT_PLAYER = "player";
+    const EVENT_FOLLOWER0 = "follower0";
+    const EVENT_FOLLOWER1 = "follower1";
+    const EVENT_FOLLOWER2 = "follower2";
+    const VEHICLE_BOAT = "boat";
+    const VEHICLE_SHIP = "ship";
+    const VEHICLE_AIRSHIP = "airship";
+
     /**
      * character を拡張して隊列メンバーも指定できるようにしたもの。
      * @param {Game_Interpreter} interpreter インタプリタ
@@ -128,49 +142,40 @@
      * @returns {Game_CharacterBase}
      */
     function getEventById( interpreter, id ) {
-        if( id < -1 ) {
-            return $gamePlayer.followers().follower( -2 - id );			// 隊列メンバー(0〜2)
+        if( id <= VEHICLE_OFFSET ) {
+            return $gameMap._vehicles[ VEHICLE_OFFSET - id ];			// 乗り物(0〜2)
+        } else if( id <= FOLLOWER_OFFSET ) {
+            return $gamePlayer.followers().follower( FOLLOWER_OFFSET - id );			// 隊列メンバー(0〜2)
         } else {
             return interpreter.character( id );			// プレイヤーキャラおよびイベント
         }
     }
 
-
-    const EVENT_THIS = "this";
-    const EVENT_SELF = "self";
-    const EVENT_PLAYER = "player";
-    const EVENT_FOLLOWER0 = "follower0";
-    const EVENT_FOLLOWER1 = "follower1";
-    const EVENT_FOLLOWER2 = "follower2";
     /**
      * 文字列をイベントIDへ変換
      * @param {String} value イベントIDの番号か識別子
      * @returns {Number} 拡張イベントID
      */
     function stringToEventId( value ) {
-        // value = treatValue( value );
+        value = treatValue( value );
         const result = parseInt( value, 10 );
         if( !isNaN( result ) ) return result;
 
         const lowValue = value.toLowerCase();
         switch( lowValue ) {
-            case EVENT_THIS:
-            case EVENT_SELF: return 0;
+            case EVENT_THIS: return 0;
             case EVENT_PLAYER: return -1;
-            case EVENT_FOLLOWER0: return -2;
-            case EVENT_FOLLOWER1: return -3;
-            case EVENT_FOLLOWER2: return -4;
+            case EVENT_FOLLOWER0: return FOLLOWER_OFFSET;
+            case EVENT_FOLLOWER1: return FOLLOWER_OFFSET - 1;
+            case EVENT_FOLLOWER2: return FOLLOWER_OFFSET - 2;
+            case VEHICLE_BOAT: return VEHICLE_OFFSET;
+            case VEHICLE_SHIP: return VEHICLE_OFFSET - 1;
+            case VEHICLE_AIRSHIP: return VEHICLE_OFFSET - 2;
         }
 
-        // イベント名で指定できるようにする
-        const i = $gameMap._events.findIndex( event => {
-            if( event === undefined ) return false;	// _events[0] が undefined なので無視
-
-            const eventId = event._eventId;
-            return $dataMap.events[ eventId ].name === value;
-        } );
-        if( i === -1 ) throw Error( `I can't find the event '${name}'` );
-        return i;
+        const e = $dataMap.events.find( e => e && e.name === value );
+        if( e === undefined ) throw Error( `${PLUGIN_NAME}: I can't find the event '${value}'` );
+        return e.id;
     }
 
 
@@ -215,6 +220,8 @@
         const jsonData = getCharacterJson( tc );
         if( jsonData ) {
             return PluginManagerEx.findMetaValue( jsonData, TF_SHADOW );
+        } else if( tc instanceof Game_Follower ) {
+            return undefined;   // Actor未設定のGame_Follower
         } else {
             return false;   // メモ欄を持たないデータ(Game_Vehicle)
         }
@@ -234,7 +241,7 @@
             const actor = tc.actor();
             if( actor ) return actor.actor();
         }
-        return;   // メモ欄を持たないデータ(Game_Vehicle)
+        return;   // メモ欄を持たないデータ
     }
 
     /**
@@ -262,7 +269,16 @@
     }
 
 
-    /*--- Game_Event ---*/
+
+    /*--- Game_Interpreter ---*/
+    const _Game_Interpreter_command129 = Game_Interpreter.prototype.command129;
+    Game_Interpreter.prototype.command129 = function( params ) {
+        const followers = $gamePlayer.followers().data();
+        followers.forEach( e => e.refreshShadow = true );
+        return _Game_Interpreter_command129.apply( this, arguments );
+    };
+
+    /*--- Game_CharacterBase ---*/
     /**
      * 影のON/OFF
      * @param {Boolean} hasShadow 影を持つか
@@ -357,6 +373,7 @@
 
         refresh() {
             this.bitmap.clear();
+            this.initRadius();
             this.initMembers();
             this.create();
         }
