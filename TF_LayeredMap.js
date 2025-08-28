ed50d1041483a18f30dfb1bbe7574aa2b8926eca
大串 敏史 <webmaster@tonbi.jp>
2025-08-28 16:23:23 +0900

    縦画面がタイル数で割り切れる場合への対処
    またTilemap初期化時にthis.tileHeightが初期値のままなのにも対処

--- TF_LayeredMap.js
+++ TF_LayeredMap.js
@@ -1,6 +1,6 @@
 //=================================================
 // TF_LayeredMap.js
-// Version :1.1.0.0
+// Version :1.1.1.0
 // For : RPGツクールMZ (RPG Maker MZ)
 // ----------------------------------------------
 // Copyright : Tobishima-Factory 2018-2025
@@ -259,7 +259,9 @@
     const _Tilemap_createLayers = Tilemap.prototype._createLayers;
     Tilemap.prototype._createLayers = function() {
         _Tilemap_createLayers.call( this );
-        const maxBillboard = Math.ceil( this._height / this.tileHeight ) + 4;  // 縦タイル数とスクロール時に必要になる+4
+        // この時点の this.tileHeight は初期値の 48 が入っているので $gameMap.tileHeight()を使う
+        // 縦タイル数とスクロール時に必要になる+5
+        const maxBillboard = Math.ceil( this._height / $gameMap.tileHeight() ) + 5;
         if( !this.hasOwnProperty( "_billboards" ) ) {
             this._billboards = [];
         }
